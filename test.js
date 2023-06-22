3aab486294f3ca4a4633b225df828b069fff20e1
aaawwwws <adeqa1@icloud.com>
2023-06-23 05:59:41 +0900

    for文の中のjson出力を外に出して挙動をちゃんとした

--- test.js
+++ test.js
@@ -1,20 +1,15 @@
 const puppeteer = require("puppeteer");
+const fs = require("fs");
 const URL = "https://kakaku.com/pc/videocard/itemlist.aspx";
 // ブラウザの設定を指定
 (async () => {
   // ブラウザの設定
   const browser = await puppeteer.launch({
     // ブラウザの設定
-    headless: true, // 動作確認するためheadlessモードにしない
+    headless: true, //ウィンドウを表示させるかどうか
   });
   // 新しいタブを開く
   const page = await browser.newPage();
-
-  // これ指定する必要があるのかわからない
-  await page.setViewport({
-    width: 1920,
-    height: 1080,
-  });
 
   // ページを価格.comに移動
   await page.goto(URL);
@@ -23,16 +18,25 @@ const URL = "https://kakaku.com/pc/videocard/itemlist.aspx";
   // class全部選択
   const GPU_Titles = await page.$$(".ckitanker");
   const GPU_Values = await page.$$(".pryen");
+  // リストを作成
+  let dates = [];
   // ここでクラス分回す
   for (let i = 0; i < GPU_Titles.length; i++) {
-    const test = await (
+    const GPU_title = await (
       await GPU_Titles[i].getProperty("textContent")
     ).jsonValue();
-    const test1 = await (
+    const GPU_Value = await (
       await GPU_Values[i].getProperty("textContent")
     ).jsonValue();
-    console.log(test + ":" + test1);
-  }
 
+    dates.push({
+      GPU: GPU_title,
+      Value: GPU_Value,
+    });
+    // console.log(GPU_title + ":" + GPU_Value);
+  }
+  // jsonに出力
+  fs.appendFileSync("./export.json", JSON.stringify(dates, null, 2));
+  // 最後にブラウザを閉じる
   await browser.close();
 })();
