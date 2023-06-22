8b0ca0e232fe032869412a52debfd3df3e20fe06
aaawwwws <adeqa1@icloud.com>
2023-06-22 19:38:53 +0900

    forにまとめた

--- test.js
+++ test.js
@@ -5,7 +5,7 @@ const URL = "https://kakaku.com/pc/videocard/itemlist.aspx";
   // ブラウザの設定
   const browser = await puppeteer.launch({
     // ブラウザの設定
-    headless: false, // 動作確認するためheadlessモードにしない
+    headless: true, // 動作確認するためheadlessモードにしない
   });
   // 新しいタブを開く
   const page = await browser.newPage();
@@ -19,15 +19,19 @@ const URL = "https://kakaku.com/pc/videocard/itemlist.aspx";
   // ページを価格.comに移動
   await page.goto(URL);
   // 表示されるまで待つ
-  await page.waitForSelector(".ckitemLink");
+  await page.waitForSelector(".ckitemLink", "pryen");
   // class全部選択
   const GPU_Titles = await page.$$(".ckitanker");
+  const GPU_Values = await page.$$(".pryen");
   // ここでクラス分回す
-  for (const GPU_Title of GPU_Titles) {
-    // クラスをテキスト化
-    const test = await (await GPU_Title.getProperty("textContent")).jsonValue();
-    // 最終的に出力
-    await console.log(test);
+  for (let i = 0; i < GPU_Titles.length; i++) {
+    const test = await (
+      await GPU_Titles[i].getProperty("textContent")
+    ).jsonValue();
+    const test1 = await (
+      await GPU_Values[i].getProperty("textContent")
+    ).jsonValue();
+    console.log(test + ":" + test1);
   }
 
   await browser.close();
