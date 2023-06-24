780671aa5917323e752ea638258a1f708a4f11ec
aaawwwws <adeqa1@icloud.com>
2023-06-24 16:59:29 +0900

    クラスの追加と多少のリファクタリング

--- test.js
+++ test.js
@@ -1,42 +1,66 @@
 const puppeteer = require("puppeteer");
 const fs = require("fs");
-const URL = "https://kakaku.com/pc/videocard/itemlist.aspx";
 // ブラウザの設定を指定
-(async () => {
-  // ブラウザの設定
-  const browser = await puppeteer.launch({
-    // ブラウザの設定
-    headless: true, //ウィンドウを表示させるかどうか
-  });
-  // 新しいタブを開く
-  const page = await browser.newPage();
 
-  // ページを価格.comに移動
-  await page.goto(URL);
-  // 表示されるまで待つ
-  await page.waitForSelector(".ckitemLink", "pryen");
-  // class全部選択
-  const GPU_Titles = await page.$$(".ckitanker");
-  const GPU_Values = await page.$$(".pryen");
-  // リストを作成
-  let dates = [];
-  // ここでクラス分回す
-  for (let i = 0; i < GPU_Titles.length; i++) {
-    const GPU_title = await (
-      await GPU_Titles[i].getProperty("textContent")
-    ).jsonValue();
-    const GPU_Value = await (
-      await GPU_Values[i].getProperty("textContent")
-    ).jsonValue();
-
-    dates.push({
-      GPU: GPU_title,
-      Value: GPU_Value,
-    });
-    // console.log(GPU_title + ":" + GPU_Value);
+// クラスを作成
+class Scraping {
+  constructor(url, parts) {
+    // コンストラクタで変更する値を設定
+    this.url = url;
+    this.parts = parts;
   }
-  // jsonに出力
-  fs.appendFileSync("./export.json", JSON.stringify(dates, null, 2));
-  // 最後にブラウザを閉じる
-  await browser.close();
-})();
+  async sc() {
+    // ブラウザの設定
+    const browser = await puppeteer.launch({
+      // ブラウザの設定
+      headless: false, //ウィンドウを表示させるかどうか
+    });
+    // 新しいタブを開く
+    const page = await browser.newPage();
+
+    // ページを価格.comに移動
+    await page.goto(this.url);
+    // 表示されるまで待つ
+    await page.waitForSelector(".ckitemLink", "pryen");
+    // class全部選択
+    const Parts_Titles = await page.$$(".ckitanker");
+    const Parts_Value = await page.$$(".pryen");
+    // リストを作成
+    let dates = [];
+    // ここでクラス分回す
+    for (let i = 0; i < Parts_Titles.length; i++) {
+      const Part_Titles = await (
+        await Parts_Titles[i].getProperty("textContent")
+      ).jsonValue();
+      const Part_Values = await (
+        await Parts_Value[i].getProperty("textContent")
+      ).jsonValue();
+
+      dates.push({
+        GPU: Part_Titles,
+        Value: Part_Values,
+      });
+      // console.log(Parts_Titles + ":" + Parts_Values);
+    }
+    // jsonに出力
+    fs.writeFileSync(
+      "./" + this.parts + "export.json",
+      JSON.stringify(dates, null, 2)
+    );
+    console.log(dates);
+    // 最後にブラウザを閉じる
+    await browser.close();
+  }
+}
+
+const func = () => {
+  const GPU_SC = new Scraping(
+    "https://kakaku.com/pc/videocard/itemlist.aspx",
+    "GPU"
+  );
+  const CPU_SC = new Scraping("https://kakaku.com/pc/cpu/itemlist.aspx", "CPU");
+  GPU_SC.sc();
+  CPU_SC.sc();
+};
+
+func();
